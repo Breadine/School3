80876f0e9423d510110b63f175b563919b30b9e9
UTENTE 29 <UTENTE 29@PC29-LAB3>
2023-12-15 12:43:12 +0100

    Aggiungere i file di progetto.

---
+++ SchoolGame/Personaggio.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolGame
+{
+    public class Personaggio
+    {
+        public int PuntiSalute { get; set; }
+
+        public string Squadra { get; set;}
+
+        public void Attacca()
+        {
+
+        }
+    }
+}
---
+++ SchoolGame/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SchoolGame;
+
+Console.WriteLine("Hello, World!");
+
+
+Cavaliere cavRosso1= new Cavaliere();
+cavRosso1.PuntiSalute = 100;
+
+Cavaliere cavVerde1 = new Cavaliere();
+cavVerde1.PuntiSalute = 100;
+
+
+cavRosso1.Attacca(cavVerde1, 20);
+
+Console.WriteLine($"Cav verde ha ora {cavVerde1.PuntiSalute} punti salute");
