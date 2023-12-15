766b0dbedf35f70048eb9b2f2f4edfc3694bf441
Breadine <marcowang936@gmail.com>
2023-12-15 13:54:03 +0100

    He

---
+++ SchoolGame/Goblin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolGame
+{
+    public class Goblin:Personaggio
+    {
+        public void Attaca(Cavaliere bersaglio, int puntiAttacco)
+        {
+            bersaglio.PuntiSalute -= this.puntiAttacco;
+        }
+    }
+}
